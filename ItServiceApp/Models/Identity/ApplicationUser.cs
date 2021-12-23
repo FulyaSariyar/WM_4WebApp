8b895314b797d38dbd890ba35c7ca32b4b893df6
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-12-23 12:06:53 +0300

    dependency Injection & aspnet Identity

---
+++ ItServiceApp/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ItServiceApp.Models.Identity
+{
+    public class ApplicationUser :IdentityUser
+    {
+        [StringLength(50)]
+        public string name { get; set; }
+        [StringLength(50)]
+        public string Surname { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    }
+}
