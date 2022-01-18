f3bf29e499f6e967f343b23fb7c99f8eea774168
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2022-01-18 12:00:15 +0300

    dxgrid ok

--- ItServiceApp/Areas/Admin/Controllers/UserApiController.cs
+++ ItServiceApp/Areas/Admin/Controllers/UserApiController.cs
@@ -1,30 +1,31 @@
-﻿using ItServiceApp.Models.Identity;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using ItServiceApp.Models.Identity;
 using ItServiceApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace ItServiceApp.Areas.Admin.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
+    [Authorize(Roles = "Admin")]
     public class UserApiController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        
+
         public UserApiController(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
         }
+
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public IActionResult GetUsers()
         {
-            var users = _userManager.Users.OrderBy(x =>
-              x.CreatedDate).ToList();
+            var users = _userManager.Users.OrderBy(x => x.CreatedDate).ToList();
 
             return Ok(users);
         }
@@ -32,20 +33,21 @@ namespace ItServiceApp.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetTest()
         {
-
             var users = new List<UserProfileViewModel>();
-            for (int i=0; i<10000; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 users.Add(new()
                 {
-                    Email="Deneme"+i,
-                    Surname="soyad"+i,
-                    Name="ad"+1
+                    Email = "Deneme" + i,
+                    Surname = "soyad" + i,
+                    Name = "ad" + i
                 });
-                
             }
-            return Ok(users);
-        }
 
+            return Ok(new JsonResponseViewModel()
+            {
+                Data = users
+            });
+        }
     }
 }
